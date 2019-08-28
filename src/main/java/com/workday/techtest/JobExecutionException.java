f06811fc210f9c8522fe609774a9cc90f2616149
rcp000j <Ramesh.Pati@walmartlabs.com>
2019-08-28 20:39:17 +0530

    added exception class

---
+++ src/main/java/com/workday/techtest/JobExecutionException.java
@@ -0,0 +1,19 @@
+package com.workday.techtest;
+
+public class JobExecutionException extends RuntimeException{
+
+	private static final long serialVersionUID = 7696528072134239344L;
+
+	public JobExecutionException() {
+		super();
+	}
+
+	public JobExecutionException(String message, Throwable cause) {
+		super(message, cause);
+	}
+
+	public JobExecutionException(String message) {
+		super(message);
+	}
+	
+}
